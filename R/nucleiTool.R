078b8a48bcb671ff739d358f02e91ec1f872574b
Yinni Kuang <kareen.kuang@mail.utoronto.ca>
2021-11-15 20:39:28 -0500

    Version 2 of nuclei feature plotting functions in nucleiTool

--- R/nucleiTool.R
+++ R/nucleiTool.R
@@ -78,36 +78,35 @@ countNuclei <- function(img){
   cat('\n', 'Number of nuclei in this image =', nbnuclei,'\n')
   return(nbnuclei)
 }
-#' Count the number of distinct objects in an image (nuclei)
+#' Generate a data frame of nuclei shape and size features
 #'
-#' The following function uses several functions from \code{\link[EBImage]} to
-#' perform counting of cell nuclei in selected image. *Frame of only nuclei is
-#' recommended (typically frame #3), counting accuracy decreases if composite
-#' image is used.
+#' The following function uses \code{computeFunction} and \code{computeFunction.shape}
+#' from \code{\link[EBImage]} tp produce a data frame containing the following features:
+#' area, perimeter, mean radius, and eccentricity. ( elliptical eccentricity defined by
+#' sqrt(1-minoraxis^2/majoraxis^2). This value approaches 0 for rounder objects and 1 for
+#' elongated objects.
 #'
-#' When it comes to counting cellular objects (most commonly nuclei),
-#' The key steps are:
-#' - enhance image if objects have week signals or ill-defined edges
-#'   (e.g. blurring turns nuclei into identifiable 'blobs')
-#' - apply a threshold to turn Greyscale image binary so every pixel is either 0 or 1
-#' - count objects in the foreground (with pixel value of 1)
+#' *Note Frame of only nuclei (typically frame #3 of composite image) is needed, since
+#' feature data extraction can only be performed on single-frame Grayscale images with
+#' defined binary objects.
 #'
 #' @param img A single-frame Grayscale \code{Image} containing ONLY the nuclei.
 #'
-#' @return Returns a \code{numeric} value of the number of nuclei counted.
+#' @return Returns a \code{data frame} containing area, perimeter, mean radius, and
+#' eccentricity of each nuclei objects in the \code{Image}.
 #'
 #' @examples
 #' # Example 1
 #' rabbit = readImage(system.file('extdata/Rabbit_01.tif', package='MyoManager'))
 #' rNuc = selectFrame(rabbit, 3)
-#' countNuclei(rNuc)
-#' viewImage(rNuc)
+#' rNucFeatures_df = getFeatureData(rNuc)
+#' View(rNucFeatures_df)
 #'
 #' # Example 2
-#' mouse = readImage(system.file('extdata/Mouse_01.tif', package='MyoManager'))
+#' mouse = readImage(system.file('extdata/Mouse_01.tiff', package='MyoManager'))
 #' mNuc = selectFrame(mouse, 3)
-#' countNuclei(mNuc)
-#' viewImage(mNuce)
+#' mNucFeatures_df = getFeatureData(mNuc)
+#' View(mNucFeatures_df)
 #'
 #' @references
 #'Gregoire Pau, Florian Fuchs, Oleg Sklyar, Michael Boutros, and Wolfgang Huber
@@ -116,13 +115,8 @@ countNuclei <- function(img){
 #'\href{https://pubmed.ncbi.nlm.nih.gov/20338898/}{link}
 #'\url{https://bioconductor.org/packages/release/bioc/html/EBImage.html}
 #'
-#'Xiaolu Yang, Xuanjing Shen, Jianwu Long, Haipeng Chen,
-#'(2012): An Improved Median-based Otsu Image Thresholding Algorithm,
-#'\emph{AASRI Procedia},Volume 3, pp. 468-473,
-#'\href{https://doi.org/10.1016/j.aasri.2012.11.074}{link}
-#'\url{https://www.sciencedirect.com/science/article/pii/S2212671612002338}
-#'
-#' @importFrom EBImage thresh opening fillHull bwlabel
+#' @importFrom EBImage Image otsu fillHull bwlabel computeFeatures computeFeatures.shape
+#' @importFrom dplyr arrange
 #' @export
 getFeatureData <- function(img){
 
@@ -130,80 +124,49 @@ getFeatureData <- function(img){
   validImage(img)
 
   # ensure nuclei image is in Grayscale
-  if (colorMode(nuc)!=0){
-    colorMode(nuc) <- 0
+  if (colorMode(img)!=0){
+    colorMode(img) <- 0
   }
   # Retrieve feature data from a binary mask of nuclei objects
-  nmask = nuc > otsu(img, range = c(-1, 2))
+  nmask = img > otsu(img, range = c(-1, 2))
   nmask = fillHull(nmask)
   nmask = bwlabel(nmask)
   # get shape feature: eccentricity
   general <- computeFeatures(nmask, img)
-  shape <- as.data.frame(rNuc_general[,4])
-  colnames(rNuc_shape) <- 's.eccentricity'
+  shape <- as.data.frame(general[,4])
+  colnames(shape) <- 's.eccentricity'
   # get size features: area, perimeter, and mean radius
   size <- computeFeatures.shape(nmask)
-  size <- as.data.frame(rNuc_size[,-4:-6])
+  size <- as.data.frame(size[,-4:-6])
 
   # combine shape and size together
-  ss <- cbind(rNuc_size, rNuc_shape)
+  ss <- cbind(size, shape)
   # filter out objects that are too small
-  ss_filtered <- subset(ss_filtered, ss_filtered$s.area>1)
+  ss_filtered <- subset(ss, ss$s.area>1)
   # re-arrange nuclei data by area (small -> large)
-  ss_filtered <- arrange(rNuc_filtered, rNuc_filtered$s.area)
-
-  return(ss_filtered)
-  # Retrieve feature data from a binary mask of nuclei objects
-  nmask = nuc > otsu(img, range = c(-1, 2))
-  nmask = fillHull(nmask)
-  nmask = bwlabel(nmask)
-  # get shape feature: eccentricity
-  general <- computeFeatures(nmask, img)
-  shape <- as.data.frame(rNuc_general[,4])
-  colnames(rNuc_shape) <- 's.eccentricity'
-  # get size features: area, perimeter, and mean radius
-  size <- computeFeatures.shape(nmask)
-  size <- as.data.frame(rNuc_size[,-4:-6])
-
-  # combine shape and size together
-  ss <- cbind(rNuc_size, rNuc_shape)
-  # filter out objects that are too small
-  ss_filtered <- subset(ss_filtered, ss_filtered$s.area>1)
-  # re-arrange nuclei data by area (small -> large)
-  ss_filtered <- arrange(rNuc_filtered, rNuc_filtered$s.area)
+  ss_filtered <- arrange(ss_filtered, ss_filtered$s.area)
 
   return(ss_filtered)
 }
-#' Count the number of distinct objects in an image (nuclei)
+#' Plot a shape/size feature of nuclei objects.
 #'
-#' The following function uses several functions from \code{\link[EBImage]} to
-#' perform counting of cell nuclei in selected image. *Frame of only nuclei is
-#' recommended (typically frame #3), counting accuracy decreases if composite
-#' image is used.
+#' The following function plots the density distribution of one of four shape/size features
+#' of cell nuceli computed from getFeatureData(): area, perimeter, radius, or eccentricity.
 #'
-#' When it comes to counting cellular objects (most commonly nuclei),
-#' The key steps are:
-#' - enhance image if objects have week signals or ill-defined edges
-#'   (e.g. blurring turns nuclei into identifiable 'blobs')
-#' - apply a threshold to turn Greyscale image binary so every pixel is either 0 or 1
-#' - count objects in the foreground (with pixel value of 1)
-#'
-#' @param img A single-frame Grayscale \code{Image} containing ONLY the nuclei.
-#'
-#' @return Returns a \code{numeric} value of the number of nuclei counted.
+#' @param featureDF A \code{data frame} containing four columns of shape/size features for
+#' each nuclei objects in an \code{Image}.
+#' @param feature A \code{character} vector indicating which feature to plot, can be
+#' area, perimeter, radius, or roundness.
 #'
 #' @examples
 #' # Example 1
 #' rabbit = readImage(system.file('extdata/Rabbit_01.tif', package='MyoManager'))
 #' rNuc = selectFrame(rabbit, 3)
-#' countNuclei(rNuc)
-#' viewImage(rNuc)
+#' df = getFeatureData(rNuc)
+#' plotFeature(df, "area")
 #'
 #' # Example 2
-#' mouse = readImage(system.file('extdata/Mouse_01.tif', package='MyoManager'))
-#' mNuc = selectFrame(mouse, 3)
-#' countNuclei(mNuc)
-#' viewImage(mNuce)
+#' plotFeature(df, "roundness")
 #'
 #' @references
 #'Gregoire Pau, Florian Fuchs, Oleg Sklyar, Michael Boutros, and Wolfgang Huber
@@ -212,18 +175,12 @@ getFeatureData <- function(img){
 #'\href{https://pubmed.ncbi.nlm.nih.gov/20338898/}{link}
 #'\url{https://bioconductor.org/packages/release/bioc/html/EBImage.html}
 #'
-#'Xiaolu Yang, Xuanjing Shen, Jianwu Long, Haipeng Chen,
-#'(2012): An Improved Median-based Otsu Image Thresholding Algorithm,
-#'\emph{AASRI Procedia},Volume 3, pp. 468-473,
-#'\href{https://doi.org/10.1016/j.aasri.2012.11.074}{link}
-#'\url{https://www.sciencedirect.com/science/article/pii/S2212671612002338}
-#'
-#' @importFrom EBImage thresh opening fillHull bwlabel
+#' @importFrom ggplot2 ggplot geom_density ggtitle theme labs
 #' @export
 plotFeature <- function(featureDF, feature = c("area", "perimeter", "radius", "roundness")){
 
   # check if features are in a data frame
-  if(!is.data.frame(feature_df)||length(feature_df)!=4){
+  if(!is.data.frame(featureDF)||length(featureDF)!=4){
     stop(
       paste("featureDF must be a data frame of features generated by featureData()")
     )
@@ -234,7 +191,7 @@ plotFeature <- function(featureDF, feature = c("area", "perimeter", "radius", "r
     stop(
       paste("please specify a feature, can be 'area', 'perimeter', 'radius', or 'roundness'.")
     )
-  } else if (feature %in% list("area", "perimeter", "radius", "roundness")){
+  } else if (!feature %in% list("area", "perimeter", "radius", "roundness")){
     stop(
       paste("feature must be one of 'area', 'perimeter', 'radius', or 'roundness'.")
     )
@@ -253,7 +210,7 @@ plotFeature <- function(featureDF, feature = c("area", "perimeter", "radius", "r
   } else if(feature=="perimeter"){
     plot <- featureDF %>%
       ggplot( aes(x = s.perimeter, y = ..density..)) + labs(x = "object perimeter") +
-      geom_density(fill="#69b3a2", color="#e9ecef", alpha=0.8) +
+      geom_density(fill="#9ecae1", color="#9ecae1", alpha=0.8) +
       ggtitle("Distribution of nuclei perimeter") +
       theme(plot.margin = margin(0.4, 0.4, 0.4, 0.4, "cm"),
             plot.background = element_rect(
@@ -261,8 +218,8 @@ plotFeature <- function(featureDF, feature = c("area", "perimeter", "radius", "r
               size = 1))
   } else if(feature=="radius"){
     plot <- featureDF %>%
-      ggplot( aes(x = s.radius, y = ..density..)) + labs(x = "object mean radius") +
-      geom_density(fill="#69b3a2", color="#e9ecef", alpha=0.8) +
+      ggplot( aes(x = s.radius.mean, y = ..density..)) + labs(x = "object mean radius") +
+      geom_density(fill="#feb24c", color="#feb24c", alpha=0.8) +
       ggtitle("Distribution of nuclei mean radius") +
       theme(plot.margin = margin(0.4, 0.4, 0.4, 0.4, "cm"),
             plot.background = element_rect(
@@ -272,7 +229,7 @@ plotFeature <- function(featureDF, feature = c("area", "perimeter", "radius", "r
     plot <- featureDF %>%
       ggplot( aes(x = s.eccentricity, y = ..density..)) +
       labs(x = "object ecc value", subtitle = "(0 = perfect circle)") +
-      geom_density(fill="#69b3a2", color="#e9ecef", alpha=0.8) +
+      geom_density(fill="#fa9fb5", color="#fa9fb5", alpha=0.8) +
       ggtitle("Distribution of nuclei eccentricity") +
       theme(plot.margin = margin(0.4, 0.4, 0.4, 0.4, "cm"),
             plot.background = element_rect(
@@ -281,36 +238,27 @@ plotFeature <- function(featureDF, feature = c("area", "perimeter", "radius", "r
   }
   plot
 }
-#' Count the number of distinct objects in an image (nuclei)
+#' Plot a matrix of pariwise shape/size feature of nuclei objects.
 #'
-#' The following function uses several functions from \code{\link[EBImage]} to
-#' perform counting of cell nuclei in selected image. *Frame of only nuclei is
-#' recommended (typically frame #3), counting accuracy decreases if composite
-#' image is used.
+#' The following function plots a matrix of pairwise scatter plots and density distribution
+#' of the four shape/size features of cell nuclei computed from getFeatureData():
+#' area, perimeter, radius, and eccentricity.
 #'
-#' When it comes to counting cellular objects (most commonly nuclei),
-#' The key steps are:
-#' - enhance image if objects have week signals or ill-defined edges
-#'   (e.g. blurring turns nuclei into identifiable 'blobs')
-#' - apply a threshold to turn Greyscale image binary so every pixel is either 0 or 1
-#' - count objects in the foreground (with pixel value of 1)
-#'
-#' @param img A single-frame Grayscale \code{Image} containing ONLY the nuclei.
-#'
-#' @return Returns a \code{numeric} value of the number of nuclei counted.
+#' @param featureDF A \code{data frame} containing four columns of shape/size features for
+#' each nuclei objects in an \code{Image}.
 #'
 #' @examples
 #' # Example 1
 #' rabbit = readImage(system.file('extdata/Rabbit_01.tif', package='MyoManager'))
 #' rNuc = selectFrame(rabbit, 3)
-#' countNuclei(rNuc)
-#' viewImage(rNuc)
+#' rNuc_df = getFeatureData(rNuc)
+#' plotFeatureMatrix(rNuc_df)
 #'
 #' # Example 2
 #' mouse = readImage(system.file('extdata/Mouse_01.tif', package='MyoManager'))
 #' mNuc = selectFrame(mouse, 3)
-#' countNuclei(mNuc)
-#' viewImage(mNuce)
+#' mNuc_df = getFeatureData(mNuc)
+#' plotFeatureMatrix(mNuc_df)
 #'
 #' @references
 #'Gregoire Pau, Florian Fuchs, Oleg Sklyar, Michael Boutros, and Wolfgang Huber
@@ -319,29 +267,28 @@ plotFeature <- function(featureDF, feature = c("area", "perimeter", "radius", "r
 #'\href{https://pubmed.ncbi.nlm.nih.gov/20338898/}{link}
 #'\url{https://bioconductor.org/packages/release/bioc/html/EBImage.html}
 #'
-#'Xiaolu Yang, Xuanjing Shen, Jianwu Long, Haipeng Chen,
-#'(2012): An Improved Median-based Otsu Image Thresholding Algorithm,
-#'\emph{AASRI Procedia},Volume 3, pp. 468-473,
-#'\href{https://doi.org/10.1016/j.aasri.2012.11.074}{link}
-#'\url{https://www.sciencedirect.com/science/article/pii/S2212671612002338}
-#'
-#' @importFrom EBImage thresh opening fillHull bwlabel
+#' @importFrom GGally ggpairs
 #' @export
 plotFeatureMatrix <- function(featureDF){
 
   # check if features are in a data frame
-  if(!is.data.frame(feature_df)||length(feature_df)!=4){
+  if(!is.data.frame(featureDF)||length(featureDF)!=4){
     stop(
       paste("featureDF must be a data frame of features generated by featureData()")
     )
-  }
+  } else(
+    # consider adding more detailed prints of what image displays
+    cat(paste("Generating a matrix of pairwise scatter and density plots.",
+              "See upper panel for correlation values of nuclei shape/size features.",
+              " "
+              ,sep="\n"))
+  )
 
   # plot pairwise scatter matrix with correlation labels
-  plot <- featureDF %>%
+  featureDF %>%
     ggpairs(columns = c("s.area", "s.perimeter", "s.radius.mean", "s.eccentricity"),
             title = "Nuceli Morphology Matrix",
             columnLabels = c("Area","Perimeter","Radius","Eccentricity"),
             upper = list(continuous = wrap('cor', size=4)))
-  plot
 }
 # [END]
