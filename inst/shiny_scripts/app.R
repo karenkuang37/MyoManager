d9bbd5ae975058160437415d6786e0b317413dce
Yinni Kuang <kareen.kuang@mail.utoronto.ca>
2021-12-05 22:59:32 -0500

    Updated app structure. Discovered image display issue, yet to be solved.

--- inst/shiny_scripts/app.R
+++ inst/shiny_scripts/app.R
@@ -6,18 +6,17 @@ ui <- navbarPage(title = "MyoManager",
                  # Tab 1: Load & Display
                  tabPanel(title = "Home",
                           titlePanel("Welcome to MyoManager!"),
-                          helpText("This is the page where you can upload and preview your
+                          tags$p("This is the page where you can upload and preview your
                                     microscopy image."),
-
                           sidebarLayout(
                             sidebarPanel(
                               tags$p("Note: valid input formats include tif, tiff, jpg, and png"),
-                              fileInput(inputId = "localInput",
-                                        label = "Choose an image or image folder from local directory"),
-                              textInput(inputId = "htmlInput",
-                                        label = "Alternatively, choose an image from url"),
-                              helpText("(please put url in quotations)"),
-                              selectInput("image", "Sample image:", list.files(system.file("images", package="EBImage"))),
+                              # fileInput(inputId = "local",
+                              #           label = "Choose an image or image folder from local directory"),
+                              textInput(inputId = "html",
+                                        label = "Alternatively, enter a valid file path or url"),
+                              helpText("(Try a sample link: https://user-images.githubusercontent.com/60583839/141215629-f19d4a77-c5f0-491f-9262-b22cd59739e3.jpg)"),
+                              selectInput("sample", "Sample image:", list.files(system.file('extdata', package = 'MyoManager'))),
                               tags$strong("Please select a display option"),
                               actionButton(inputId = "color",
                                            label = "Color"),
@@ -26,22 +25,80 @@ ui <- navbarPage(title = "MyoManager",
                             ),
                            # Show a display of the generated distribution
                             mainPanel(
+                              helpText("Display may take a few seconds to render"),
                               tabsetPanel(
-                                tabPanel("Static raster", plotOutput("raster")),
-                                tabPanel("Interactive browser", displayOutput("widget"))
+                                tabPanel("Static raster", plotOutput("raster_1")),
+                                tabPanel("Interactive browser", displayOutput("widget_1"))
                               )
                             )
-                          ),
+                          )
                  ),
                  # Tab 2: Image Processor
                  navbarMenu(title = "Image Processor",
-                            tabPanel(title = "Select frame"
+                            # Tab 2.1: Adjust Intensity
+                            tabPanel(title = "Adjust intensity",
+                                     titlePanel("Image Intensity Control"),
+                                     tags$p("On this page you can modify the brightess and contrast
+                                              level of the image for better viewing quality."),
+                                     helpText("(Hint: decreased brightness + increased contrast
+                                              is good for object identification)"),
+                            sidebarLayout(sidebarPanel(
+                                # for picking a frame
+                                numericInput(inputId = "fNum",
+                                             label = "Please specify a frame",
+                                             helpText("(see Home tab for the frame number of nuclei/cell body)"),
+                                             value = 3),
+                                 # for picking brightness
+                                 numericInput(inputId = "brightness",
+                                             label = "Please specify the brightness",
+                                             helpText("(e.g. -0.2)"),
+                                             value = -0.2),
+                                 # for picking contrast
+                                 numericInput(inputId = "contrast",
+                                              label = "Please specify the contrast level",
+                                              helpText("(e.g. 3)"),
+                                              value = 3)
+                              ),
+                              mainPanel(
+                                helpText("Display may take a few seconds to render"),
+                                tabsetPanel(
+                                  tabPanel("Interactive browser", displayOutput("widget_2.1"))
+                                 )
+                               ))
                             ),
-                            tabPanel(title = "Adjust intensity"
+                            # Tab 2.2: Blurring Filter
+                            tabPanel(title = "Blurring Filter",
+                                     titlePanel("Apply a blurring filter"),
+                                     tags$p("On this page you can apply a blurring filter on
+                                              selected image frame,"),
+                                     helpText("Blurring tends to be useful prior to analyzing
+                                              individual objects in an image with ill-defined
+                                              edges and/or uneven intensities."),
+                            sidebarLayout(sidebarPanel(
+                                # for picking a frame
+                                numericInput(inputId = "fNum",
+                                             label = "Please specify a frame",
+                                             helpText("(see Home tab for the frame number of nuclei/cell body)"),
+                                             value = 3),
+                                # for picking brush size
+                                numericInput(inputId = "bSize",
+                                             label = "Brush size:",
+                                             helpText("(e.g.11)"),
+                                             value = 11),
+                                # for picking brush shape
+                                selectInput(inputId = "bShape",
+                                            label = "Brush shape:",
+                                            choices = list('line','box','disc','diamond','Gaussian'))
+                              ),
+                              mainPanel(
+                                helpText("Display may take a few seconds to render"),
+                                tabsetPanel(
+                                  tabPanel("Interactive browser", displayOutput("widget_2.2"))
+                                )
+                              ))
                             ),
-                            tabPanel(title = "Apply blurring filter"
-                            ),
-                            tabPanel(title = "View segmented objects"
+                            # Tab 2.3: Object Segmentation
+                            tabPanel(title = "Object Segmentation"
                             ),
                  ),
                  # Tab 3: Nuclei Counter
@@ -61,10 +118,27 @@ ui <- navbarPage(title = "MyoManager",
 )
 
 server <- function(input, output) {
-  inFile <- reactive({
-    f = system.file("images", input$image, package="EBImage")
+  # Tab 1: Load & Display
+  # Set up reactive image file
+  # inFile_local <- reactive({
+  #   if(!is.null(input$local)){
+  #     MyoManager::readImage(input$local)
+  #   }else{
+  #     NULL
+  #   }
+  # })
+  inFile_sample <- reactive({
+    f = system.file("extdata", input$sample, package="MyoManager")
     MyoManager::loadImage(f)
   })
+  inFile_html <- reactive({
+    if(!is.null(input$html)){
+      MyoManager::loadImage(input$html)
+    }else{
+      NULL
+    }
+  })
+
 
   rv <- reactiveValues(
     # display options for Tab 1
@@ -80,45 +154,43 @@ server <- function(input, output) {
     rv$colorMode <- 0})
 
   # render display output
-  output$raster <- renderPlot({
+  output$raster_1 <- renderPlot({
     if(is.null(rv$colorMode)){
       return()
     }else{
-      plot(EBImage::`colorMode<-`(inFile(), rv$colorMode), all=TRUE)
+      plot(EBImage::`colorMode<-`(inFile_sample(), rv$colorMode), all=TRUE)
+
+      # if(!is.null(isolate(inFile_html()))){
+      #   plot(EBImage::`colorMode<-`(inFile_html(), rv$colorMode), all=TRUE)
+      # }
+      # } else if(!is.null(isolate(inFile_local())) && is.null(isolate(inFile_html()))){
+      #   plot(EBImage::`colorMode<-`(inFile_local(), rv$colorMode), all=TRUE)
+      # }
+      # else {
+        # plot(EBImage::`colorMode<-`(inFile_sample(), rv$colorMode), all=TRUE)
+      # }
     }
   })
-  output$widget <- renderDisplay({
+
+  output$widget_1 <- renderDisplay({
     if(is.null(rv$colorMode)){
       return()
     }else{
-      MyoManager::viewImage(inFile(), rv$colorMode)
+      MyoManager::viewImage(inFile_sample(), rv$colorMode)
+      # if(is.null(isolate(inFile_local())) && is.null(isolate(inFile_html()))){
+      # MyoManager::viewImage(inFile_sample(), rv$colorMode)
+      # } else if(!is.null(isolate(inFile_local())) && is.null(isolate(inFile_html()))){
+      #   MyoManager::viewImage(inFile_local(), rv$colorMode)
+      # } else if(!is.null(isolate(inFile_html())) && is.null(isolate(inFile_local()))){
+      #   MyoManager::viewImage(inFile_html(), rv$colorMode)
+      # }
     }
   })
+
 
   observeEvent(input$reunif, { rv$unif <- runif(500) })
   observeEvent(input$rechisq, { rv$chisq <- rchisq(500, 2) })
 
-  # output$originalInput <- renderPlot({
-  #
-  #   local <- input$localInput
-  #   html <- input$htmlInput
-  #
-  #   if (!is.null(local) && !is.null(html)) {
-  #     print("please only select one input option")
-  #   } else if (!is.null(local)) {
-  #     inFile <- MyoManager::loadImage(local)
-  #   } else if (!is.null(html)){
-  #     inFile <- MyoManager::loadImage(html)
-  #   }
-  #
-  #   if (is.null(rv$colorMode)){
-  #     return()
-  #   }else{
-  #     #plot(inFile)
-  #     hist(1)
-  #     # return(MyoManager::viewImage(inFile, rv$colorMode))
-  #   }
-  # })
 
   output$unif <- renderPlot({
     hist(rv$unif, breaks = 30, col = "grey", border = "white",
